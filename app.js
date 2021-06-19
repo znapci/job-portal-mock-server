271de93898aa2b1ff30c371c8a6870c565923c15
znapci <znapci@gmail.com>
2021-06-19 19:43:19 +0530

    Minor refactor

--- app.js
+++ app.js
@@ -1,83 +1,83 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-//const path = require('path');
-const jdata = require('./data.json');
-const data = Array(20).fill(jdata);
-const date = new Date();
-const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+// const path = require('path');
+const jdata = require('./data.json')
+const data = Array(20).fill(jdata)
+const date = new Date()
+const LoremIpsum = require('lorem-ipsum').LoremIpsum
 
 const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-        max: 8,
-        min: 4
-    },
-    wordsPerSentence: {
-        max: 16,
-        min: 4
-    }
-});
-
-const t = [];
-data.map((post, id) => {
-    t.push({
-        ...post, id: id,
-        companyUrl: 'bhavam',
-        joinByDate: date.toDateString(),
-        applyByDate: date.toDateString(),
-        place: 'Nitte',
-        tags: ['new', 'hot', 'trending', 'in-demand', 'test'],
-        responsibilities: ['Design the system', 'Implement designs', 'Collaborate with others', 'Come up with new ideas'],
-        requirements: ['Have basic knowledge of database', 'Know React', 'Know Node.js/express.js', 'Know git-github', 'available for next 2 months'],
-        description: lorem.generateParagraphs(3)
-    })
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
 })
 
-app.use(cors());
-app.use(express.json());
+const t = data.map((post, id) =>
+  ({
+    ...post,
+    id: id,
+    companyUrl: 'bhavam',
+    joinByDate: date.toDateString(),
+    applyByDate: date.toDateString(),
+    place: 'Nitte',
+    tags: ['new', 'hot', 'trending', 'in-demand', 'test'],
+    responsibilities: ['Design the system', 'Implement designs', 'Collaborate with others', 'Come up with new ideas'],
+    requirements: ['Have basic knowledge of database', 'Know React', 'Know Node.js/express.js', 'Know git-github', 'available for next 2 months'],
+    description: lorem.generateParagraphs(3)
+  })
+)
 
-let count = 0;
+app.use(cors())
+app.use(express.json())
+
+let count = 0
 const singlePost = (req, res, next) => {
-    console.log('in ExpandedPost', req.params);
-    res.status(200).send(`Post ${req.params.id}`);
+  console.log('in ExpandedPost', req.params)
+  res.status(200).send(`Post ${req.params.id}`)
 }
 const displayPosts = (req, res, next) => {
-    console.log('no. of req to displayPosts: ', ++count);
-    res.status(200).json(t);
+  console.log('no. of req to displayPosts: ', ++count)
+  res.status(200).json(t)
 }
 const delay = (req, res, next) => {
-    setTimeout(next, 2000)
+  setTimeout(next, 2000)
 }
 const addPost = (req, res, next) => {
-    console.log(req.body);
+  console.log(req.body)
 }
 const login = (req, res, next) => {
-    console.log(req.body);//spits empty object
-    if (req.body.password === 'success')
-        res.json({
-            status: 'success',
-            token: 'lol'
-        });
-    else {
-        res.json({
-            status: 'failure',
-            token: 'lol'
-        });
-    }
+  console.log(req.body)// spits empty object
+  if (req.body.password === 'success') {
+    res.json({
+      status: 'success',
+      token: 'lol'
+    })
+  } else {
+    res.json({
+      status: 'failure',
+      token: 'lol'
+    })
+  }
 }
 const company = (req, res, next) => {
-    console.log(req.body);
-    res.redirect('http://google.com')
+  console.log(req.body)
+  res.redirect('http://google.com')
 }
 // const sendImage = (req, res, next) => {
 //     console.log('image');
 //     res.sendFile(path.join(__dirname, 'ubuntu.png'));
 // }
 
-app.get('/post/:id', singlePost);
-app.get('/posts', delay, displayPosts);
-app.post('/addpost', addPost);
+app.get('/post/:id', singlePost)
+app.get('/posts', delay, displayPosts)
+app.post('/addpost', addPost)
 app.get('/company', company)
-app.post('/login', delay, login);
+app.post('/login', delay, login)
 // app.use('/img', delay, sendImage);
-app.listen(8000);
+app.listen(8000)
